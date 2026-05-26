401fc3a984a266b2a7e08d43f277be91b827680a
lucio <luciovida08@gmail.com>
2026-05-26 16:29:14 -0300

    .

--- src/frontend-app/src/routes/perfil/+page.svelte
+++ src/frontend-app/src/routes/perfil/+page.svelte
@@ -2,10 +2,11 @@
     import { Heading } from 'flowbite-svelte';
     import Menu from '../../components/Menu.svelte';
     import {  P, ImagePlaceholder, Skeleton, TextPlaceholder } from "flowbite-svelte";
+    import { goto } from "$app/navigation";
   </script>
   
   <Menu />
-  
+  Card
   <div class="text-center p-8 pt-32">
       <Heading tag="h2" class="text-4xl font-extrabold tracking-tight  text-secondary-600 : dark:text-white mb-6">Perfil</Heading>
       <P class="text-lg leading-relaxed text-secondary-900 dark:text-gray-300 mb-4 text-justify">
@@ -15,9 +16,9 @@
       <button
       class="p-2 rounded border border-primary-500 hover:border-tertiary-50 transition bg-transparent"
       title="Editar"
-      on:click={() => goto(`/users/edit/${user.id}`)}
-    >
+      on:click={() => goto(`/users/edit/${user.id}`)}>
       oi
     </button>
+    
 </div>
   </div>
